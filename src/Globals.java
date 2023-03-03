12d94c5afb1f47fa8be6360f7ae7859f7ccd15ca
Diego <diegoyegrosa@gmail.com>
2023-03-03 09:48:55 -0300

    globals deletion

--- src/Globals.java
+++ src/Globals.java
@@ -1,5 +1,1 @@
-public class Globals {
-    public static final String MENUS = "C:\\Users\\daya\\Documents\\EjercicioTipoExamen\\archivos\\MENUS.txt";
-    public static final String BOTONES = "C:\\Users\\daya\\Documents\\EjercicioTipoExamen\\archivos\\BOTONES.txt";
-    public static final String PAGINAS = "C:\\Users\\daya\\Documents\\EjercicioTipoExamen\\archivos\\PAGINAS.txt";
-}
+public class Globals {}
